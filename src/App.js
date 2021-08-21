db8432db320be7c2ca9f58da71d5fa3c0b238c36
Arafat <arafat97alim@gmail.com>
2021-08-22 00:47:29 +0530

    conditonal rendering.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import "./App.css";
 
 function App() {
   const name = "Arafat";
+  const isLogged = true;
   return (
     <>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Hello {name}</h2>
+          {/* <h2>Hello {isLogged ? name : "World"}</h2>  */}
+          {!isLogged && <h2>Hello World</h2>}
+          {isLogged && <h2>Hello {name}</h2>}
           <p>Today is {new Date().toDateString()}</p>
         </header>
       </div>
